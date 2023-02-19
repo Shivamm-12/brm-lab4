7ff6ce9e8db8046cc713cab7dc250c646eec5c96
shivam chaudhary <shivamchaudhary@shivams-MacBook-Air-2.local>
2023-02-19 16:31:07 +0530

    first commit

---
+++ src/main/java/mypkg4/MyClass4.java
@@ -0,0 +1,16 @@
+package mypkg4;
+
+
+public class MyClass4 {
+	
+	public void show()
+	{
+		System.out.println("Hello , Welcome To Jenkins");
+	}
+	
+public static void main (String[] args)
+{
+	MyClass4 ob  = new MyClass4();
+	ob.show();
+}
+}
